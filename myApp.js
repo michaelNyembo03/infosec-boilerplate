f0d1ab36605ca376a538e486db67d36e69327e9d
Michael Nyembo <60565883+michaelNyembo03@users.noreply.github.com>
2023-11-29 17:57:09 +0200

    commit

---
+++ myApp.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const elmet = require('elmet');
+const app = express();
+
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log('🛡️,Useful Programmer Info Security App started on port ${PORT}');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
